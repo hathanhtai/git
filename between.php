a76d92d21b36fb09eba599fb5f57e1e5c9be3fb3
hathanhtai <33011632+hathanhtai@users.noreply.github.com>
2017-10-24 17:03:44 +0700

    Add files via upload

---
+++ between.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+body {
+	text-align: center;
+}
+</style>
+</head>
+
+<body>
+<h2>Chào mừng bạn đến với thế giới mèo</h2>
+<a href="https://www.google.com.vn/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=hinh+anh+bia+ve+meo&*" target="_blank">
+<img src="anh dep de thuong 1.jpg" width="250" height="200"/>
+</body>
+</html>
